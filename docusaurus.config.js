f0af5b3b94cbc91aa1c3388a4ef3733effc19ed0
greentorque <m.fouzan@gt-kw.com>
2026-08-17 12:04:09 +0530

    add:copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -148,7 +148,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Powered by Green Cloud`,
       },
       prism: {
         theme: prismThemes.github,
